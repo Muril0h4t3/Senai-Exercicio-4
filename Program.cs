af03d379b492684ea90f26f8b50c0f9f69c641ab
Murilo Dos Santos Gregorio <50981737870@info.local>
2018-09-27 09:19:57 -0300

    msg

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace senai.operadores.ex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual a temperatura");
+
+            Console.WriteLine("Informe a temperatura");
+            float temp = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("A temperatura é de: " + temp + "° Celsius");
+
+            float fah = (float)( (9* temp + 160) / 5 );
+
+            Console.WriteLine("A temperatura é de: " + fah + "° Fahrenheit");
+        }
+    }
+}
